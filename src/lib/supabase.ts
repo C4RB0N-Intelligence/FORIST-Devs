e1fd2f388dc072ba1411686ba0d31fd21cf675a1
aniket-2210 <anikets2210@gmail.com>
2026-08-12 11:43:51 +0530

    Ref: Update supabase.ts with correct URL and key

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://url.supabase.co';
-const supabaseKey = 'key';
+const supabaseUrl = 'https://mfzrfxdfegrsilthepuf.supabase.co';
+const supabaseKey = 'sb_publishable__zVIUoOt5g1-bOTuyq7LVQ_RhLeUaqh';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
